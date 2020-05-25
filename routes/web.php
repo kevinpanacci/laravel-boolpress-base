b92d52a5c70ee42ed4e42f65456d397426de99c2
Kevin <kevinpanacci@hotmail.it>
2020-05-25 14:11:18 +0200

    Aggiunte Show e Index

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts/published', 'PostController@indexPublished')->name('posts.indexPublished');
 Route::resource('posts', 'PostController');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->middleware('auth')
+->group(function() {
+    Route::resource('users', 'UserController');
+});
